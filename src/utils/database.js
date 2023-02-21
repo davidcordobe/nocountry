e8375b70a014590d5184b4441fa03399058b3158
david arce <arce475@gmail.com>
2023-02-21 12:18:43 -0300

    backend

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,7 +7,7 @@ const connectDB = async() => {
 
   mongoose.set('strictQuery', false)
 
-  const conn = await mongoose.connect(process.env.DB_MONGO_URL, {
+  const conexion = await mongoose.connect(process.env.DB_MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true,
